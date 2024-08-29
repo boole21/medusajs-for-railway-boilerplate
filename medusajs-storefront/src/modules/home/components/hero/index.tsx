6c6f642c8f795affd380bbb4fa8df7ea71ccf812
lk <nailme@sina.com>
2024-08-29 12:47:21 +0800

    feautured2

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -2,21 +2,21 @@ import { Region } from "@medusajs/medusa"
 import { Button, Heading } from "@medusajs/ui"
 import Banner from "@modules/home/components/banner"
 // import HomeCategories from "@modules/home/components/categories"
-// import HomeFeatured from "@modules/home/components/featured"
+import HomeFeatured from "@modules/home/components/featured"
 import { ProductCollectionWithPreviews } from "types/global"
 
 const Hero = ({
   region,
-}: // product_categories,
-{
+  product_categories,
+}: {
   region: Region
-  // product_categories: ProductCollectionWithPreviews[]
+  product_categories: ProductCollectionWithPreviews[]
 }) => {
   return (
     <>
       <Banner />
       {/* <HomeCategories /> */}
-      {/* <HomeFeatured region={region} product_categories={product_categories} /> */}
+      <HomeFeatured region={region} product_categories={product_categories} />
     </>
   )
 }
