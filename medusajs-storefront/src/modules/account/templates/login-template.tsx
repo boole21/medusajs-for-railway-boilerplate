ed95759824ef3e494ad204bc84be694abaa639fa
lk <nailme@sina.com>
2024-08-29 16:47:34 +0800

    account

--- medusajs-storefront/src/modules/account/templates/login-template.tsx
+++ medusajs-storefront/src/modules/account/templates/login-template.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 
 import Register from "@modules/account/components/register"
 import Login from "@modules/account/components/login"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 
 export enum LOGIN_VIEW {
   SIGN_IN = "sign-in",
@@ -14,13 +15,34 @@ const LoginTemplate = () => {
   const [currentView, setCurrentView] = useState("sign-in")
 
   return (
-    <div className="w-full flex justify-start px-8 py-8">
-      {currentView === "sign-in" ? (
-        <Login setCurrentView={setCurrentView} />
-      ) : (
-        <Register setCurrentView={setCurrentView} />
-      )}
-    </div>
+    <>
+      <section className="breadcrumb">
+        <ul className="breadcrumb__list flex container">
+          <li>
+            <LocalizedClientLink className="breadcrumb__link" href={`/`}>
+              Home
+            </LocalizedClientLink>
+          </li>
+          <li>
+            <span className="breadcrumb__link">{">"}</span>
+          </li>
+          <li>
+            <span className="breadcrumb__link">Login / Register</span>
+          </li>
+        </ul>
+      </section>
+      <div className="login-register section--lg">
+        <div className="login-register__container container grid">
+          {/* {currentView === "sign-in" ? (
+          <Login setCurrentView={setCurrentView} />
+        ) : (
+          <Register setCurrentView={setCurrentView} />
+        )} */}
+          <Login setCurrentView={setCurrentView} />
+          <Register setCurrentView={setCurrentView} />
+        </div>
+      </div>
+    </>
   )
 }
 
